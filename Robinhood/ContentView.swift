6ac68058b26504795730f56921d9a9e1fce0ed09
Janarthanan Kannan <tjana17@gmail.com>
2025-05-05 10:25:19 +0530

    Initial Commit

---
+++ Robinhood/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Robinhood
+//
+//  Created by Janarthanan Kannan on 05/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Robinhood/RobinhoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobinhoodApp.swift
+//  Robinhood
+//
+//  Created by Janarthanan Kannan on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct RobinhoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
